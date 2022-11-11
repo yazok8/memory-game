33383f309f204024d2c4415b51615f807d26eae5
yazok8 <yazankhirfan@gmail.com>
2022-11-11 12:23:47 -0500

    add check for match and restore functions to the memory game

--- app/src/main/java/com/example/trialapp/MemoryBoardAdapter.kt
+++ app/src/main/java/com/example/trialapp/MemoryBoardAdapter.kt
@@ -8,6 +8,8 @@ import android.view.ViewGroup
 import android.view.ViewGroup.MarginLayoutParams
 import android.widget.ImageButton
 import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.trialapp.models.BoardSize
 import com.example.trialapp.models.MemoryCard
@@ -56,6 +58,10 @@ class MemoryBoardAdapter(
         fun bind(position: Int) {
             val memoryCard = cards[position]
             imageButton.setImageResource(if(memoryCard.isFaceUp) memoryCard.identifier else R.drawable.ic_launcher_background)
+
+            imageButton.alpha = if(memoryCard.isMatched) .4f else 1.0f
+            val colorStateList = if(memoryCard.isMatched) ContextCompat.getColorStateList(context, R.color.color_gray) else null
+            ViewCompat.setBackgroundTintList(imageButton, colorStateList)
             imageButton.setOnClickListener{
                 Log.i(Tag, "clicked on position $position")
                 cardClickListener.onCardClicked(position)
--- app/src/main/java/com/example/trialapp/models/MemoryGame.kt
+++ app/src/main/java/com/example/trialapp/models/MemoryGame.kt
@@ -5,7 +5,9 @@ import com.example.trialapp.utils.DEFAULT_ICONS
 class MemoryGame(private val boardSize: BoardSize) {
 
     val cards: List<MemoryCard>
-    val numPairsFound = 0
+    var numPairsFound = 0
+
+    private var indexOfSingleSelectedCard: Int? = null
 
     init {
         val chosenImages = DEFAULT_ICONS.shuffled().take(boardSize.getNumPairs())
@@ -13,8 +15,42 @@ class MemoryGame(private val boardSize: BoardSize) {
         cards= randomizedImages.map {MemoryCard(it)}
     }
 
-    fun flipCard(position: Int) {
+    fun flipCard(position: Int): Boolean {
         val card = cards[position]
+        // 0 cards previously flipped over => flip over the selected card
+        // 1 card previously flipped over => flip over the selected card and check if the images match
+        // 2 cards previously flipped over => restore the cards and flip over the selected card
+        var foundMatch = false
+        if(indexOfSingleSelectedCard == null){
+            // 0 or 2 cards previously flipped over
+            restoreCards()
+            indexOfSingleSelectedCard=position
+        }else{
+            //1 card is previously flipped over
+            foundMatch = checkForMatch(indexOfSingleSelectedCard!!, position)
+            indexOfSingleSelectedCard = null
+        }
+
         card.isFaceUp = !card.isFaceUp
+        return foundMatch
+    }
+
+    private fun checkForMatch(position1: Int, position2: Int): Boolean {
+        if(cards[position1].identifier != cards[position2].identifier){
+            return false
+        }
+        cards[position1].isMatched = true
+        cards[position2].isMatched = true
+
+        numPairsFound++
+        return true
+    }
+
+    private fun restoreCards() {
+        for(card in cards){
+            if(!card.isMatched){
+            card.isFaceUp = false
+            }
+        }
     }
 }
