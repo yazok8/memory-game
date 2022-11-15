30a6cde7c5f7968b1d0b552a1ba9b8def9d5161e
yazok8 <yazankhirfan@gmail.com>
2022-11-15 15:19:26 -0500

    add read photos permission in manifest and functionality to allow user to select a photo

--- app/src/main/java/com/example/trialapp/CreateActivity.kt
+++ app/src/main/java/com/example/trialapp/CreateActivity.kt
@@ -1,25 +1,96 @@
 package com.example.trialapp
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.trialapp.models.BoardSize
 import com.example.trialapp.utils.EXTRA_BOARD_SIZE
+import com.example.trialapp.utils.isPermissionGranted
+import com.example.trialapp.utils.requestPermission
 
 class CreateActivity : AppCompatActivity() {
 
+
+    companion object {
+        private const val PICK_PHOTO_CODE = 699
+        private const val READ_EXTERNAL_PHOTOS_CODE = 248
+        private const val READ_PHOTOS_PERMISSION = android.Manifest.permission.READ_EXTERNAL_STORAGE
+    }
+
+    private lateinit var rvImagePicker: RecyclerView
+    private lateinit var etGameName: EditText
+    private lateinit var btnSave: Button
+
+
     private lateinit var boardSize: BoardSize
     private var numImagesRequired = -1
+    private val chosenImageUris = mutableListOf<Uri>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create)
 
+        rvImagePicker = findViewById(R.id.rvImagePicker)
+        etGameName = findViewById(R.id.etGameName)
+        btnSave = findViewById(R.id.btn_save)
+
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         boardSize= intent .getSerializableExtra(EXTRA_BOARD_SIZE) as BoardSize
         numImagesRequired = boardSize.getNumPairs()
         supportActionBar?.title = "Choose pics (0 / $numImagesRequired)"
+
+        rvImagePicker.adapter = ImagePickerAdapter(this, chosenImageUris, boardSize, object : ImagePickerAdapter.ImageClickListener{
+            override fun onPlaceHolderClicked() {
+                if(isPermissionGranted(this@CreateActivity,READ_PHOTOS_PERMISSION )){
+                     launchIntentForPhotos()
+                } else{
+                    requestPermission(this@CreateActivity, READ_PHOTOS_PERMISSION, READ_EXTERNAL_PHOTOS_CODE )
+                }
+            }
+
+        })
+        rvImagePicker.setHasFixedSize(true)
+        rvImagePicker.layoutManager = GridLayoutManager(this, boardSize.getWidth())
     }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if(requestCode == READ_EXTERNAL_PHOTOS_CODE){
+            if(grantResults.isNotEmpty()&& grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                launchIntentForPhotos()
+            }else{
+                Toast.makeText(this,"In order to have a custom game, our app needs a permission to access the photos from their phone", Toast.LENGTH_LONG).show()
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    private fun launchIntentForPhotos() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
+        getResult.launch(Intent.createChooser(intent, "choose pics"))
+    }
+    private val getResult =
+        registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) {
+            if (it.resultCode== PICK_PHOTO_CODE) {
+                val value = it.data?.getStringExtra("input")
+            }
+        }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if(item.itemId == android.R.id.home){
